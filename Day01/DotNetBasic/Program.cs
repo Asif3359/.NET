d0913611f8d13c66c3a1ebb6b7b942980eb498a3
Asif Ahammed <asifahammed359@gmail.com>
2026-01-03 22:37:02 +0600

    complete day2

---
+++ Day01/DotNetBasic/Program.cs
@@ -0,0 +1,54 @@
+﻿// Variables & Data Types
+int age = 24;
+double cgpa = 3.60;
+char grade = 'A';
+bool isstudent = true;
+string name = "Asif Ahammad";
+
+Console.WriteLine(name);
+Console.WriteLine(isstudent ? "student" : "Worker");
+Console.WriteLine(cgpa);
+Console.WriteLine(age);
+Console.WriteLine(grade);
+
+
+
+// Type Inference (var)
+var student = "Asif Ahammad";
+var gpa = 4.83;
+
+Console.WriteLine(student);
+Console.WriteLine(gpa);
+
+
+// Operators
+
+int a = 10;
+int b = 3;
+
+Console.WriteLine(a + b);
+Console.WriteLine(a - b);
+Console.WriteLine(a * b);
+Console.WriteLine(a / b);
+Console.WriteLine(a % b);
+
+
+// Comparison Operators
+Console.WriteLine(a > b);
+Console.WriteLine(a < b);
+Console.WriteLine(a == b);
+Console.WriteLine(a != b);
+
+
+// Logical Operators
+
+bool x = true;
+bool y = false;
+
+Console.WriteLine(x && y); // AND
+Console.WriteLine(x || y); // OR
+Console.WriteLine(!x);     // NOT
+Console.WriteLine(!y);     // NOT
+
+int futureAge = age + 5;
+Console.WriteLine($"Age after 5 years: {futureAge}");
---
+++ Day02/ControlFlow_PatternMatching_Expressions/Program.cs
@@ -0,0 +1,86 @@
+﻿// Advanced if / else (Guard Clauses)
+int marks = 60;
+string grade = "";
+
+if (marks < 0 || marks > 100)
+{
+    Console.WriteLine("Invalid Marks");
+    return;
+}
+
+if (marks >= 80)
+{
+    grade = "A+";
+    Console.WriteLine("A+");
+}
+else if (marks >= 70)
+{
+    grade = "A";
+    Console.WriteLine("A");
+}
+else if (marks >= 60)
+{
+    grade = "A-";
+    Console.WriteLine("A-");
+}
+else
+{
+    Console.WriteLine("Fail");
+}
+
+
+
+// Modern C# switch Expression (Recommended)
+
+
+string result = grade switch
+{
+    "A+" => "Excellent",
+    "A" => "Good",
+    "A-" => "Average",
+    _ => "Fail"
+};
+
+Console.WriteLine(result);
+
+
+string status = marks switch
+{
+    >= 80 => "A+",
+    >= 70 => "A",
+    >= 60 => "A-",
+    _ => "Failed"
+};
+
+Console.WriteLine(status);
+
+// Loop Best Practices (Avoid Classic Mistakes)
+
+for (int i = 0; i <= 5; i++)
+{
+    Console.WriteLine($"Iteration {i}");
+}
+
+int balance = 100;
+
+while (balance > 0)
+{
+    balance -= 20;
+    Console.WriteLine($"Balance: {balance}");
+}
+
+// foreach (MOST USED in .NET APIs)
+string[] names = { "Asif", "Rahim", "Karim" };
+
+foreach (var item in names)
+{
+    Console.WriteLine(item);
+}
+
+for (int i = 1; i <= 10; i++)
+{
+    if (i == 5) continue;
+    if (i == 9) break;
+
+    Console.WriteLine(i);
+}
