dc2765aa450b7a87c38b8fe44dd0b2171133171a
Asif Ahammed <asifahammed359@gmail.com>
2026-01-03 21:52:38 +0600

    besic variables and datatypes

--- Day01/HelloDotNet/Program.cs
+++ Day01/HelloDotNet/Program.cs
@@ -2,3 +2,14 @@
 Console.WriteLine("Hello Asif");
 Console.WriteLine("welcome to dot NET core");
 
+int age = 24;
+double cgpa = 3.60;
+char grade = 'A';
+bool isstudent = true;
+string name = "Asif Ahammad";
+
+Console.WriteLine(name);
+Console.WriteLine(isstudent ? "student" : "Worker");
+Console.WriteLine(cgpa);
+Console.WriteLine(age);
+Console.WriteLine(grade);
