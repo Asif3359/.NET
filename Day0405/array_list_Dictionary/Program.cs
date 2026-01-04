6ba543bf949f30cb4bc23cb9ae3e4be0d4b5b16a
Asif Ahammad <asifahammed359@gmailcom>
2026-01-04 17:43:43 +0600

    complete day07

---
+++ Day0405/array_list_Dictionary/Program.cs
@@ -0,0 +1,40 @@
+﻿// Arrays (Use Carefully)
+int[] scores = { 65, 78, 90, 55 };
+
+foreach (var score in scores)
+{
+    Console.WriteLine(score);
+}
+
+
+
+// Lists (MOST USED)
+List<int> marks = new() { 60, 72, 85, 49 };
+
+marks.Add(95);
+marks.Remove(49);
+
+var passed = marks.Where(m => m >= 60);
+
+foreach (var m in passed)
+{
+    Console.WriteLine(m);
+}
+
+
+// Dictionaries (Key–Value Mapping)
+Dictionary<int, string> students = new()
+{
+    { 1, "Asif" },
+    { 2, "Rahim" }
+};
+
+students[3] = "Karim";
+students.Add(4,"Adf");
+
+
+foreach (var item in students)
+{
+    Console.WriteLine($"{item.Key}, {item.Value}");
+}
+
---
+++ Day06/Program.cs
@@ -0,0 +1,53 @@
+﻿// string name = "Asif";
+// name += " Ahmed";   // ❌ new object created
+// // ❗ Bad in loops → memory + performance issues
+// Console.WriteLine(name);
+
+
+
+// String Interpolation (Best Practice)
+using System.Text;
+
+string firstName = "Asif";
+string lastName = "Ahmed";
+
+string fullName = $"{firstName} {lastName}";
+Console.WriteLine(fullName);
+
+
+string input = "  Asif Ahmed  ";
+
+Console.WriteLine(input.Trim());
+Console.WriteLine(input.ToUpper());
+Console.WriteLine(input.ToLower());
+Console.WriteLine(input.Replace("Ahmed", "A."));
+
+
+string data = "Asif,24,3.60";
+
+var parts = data.Split(',');
+
+string name = parts[0];
+
+if (int.TryParse(parts[1], out int age) &&
+    double.TryParse(parts[2], out double cgpa))
+{
+    Console.WriteLine(name);
+    Console.WriteLine(age);
+    Console.WriteLine(cgpa);
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
+
+
+
+var sb = new StringBuilder();
+
+for (int i = 0; i < 1000; i++)
+{
+    sb.Append(i);
+}
+
+string result = sb.ToString();
