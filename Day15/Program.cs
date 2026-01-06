09c270c8c4fdb7a1b571016f0712f71fecaecde2
Asif Ahammad <asifahammed359@gmailcom>
2026-01-06 12:31:46 +0600

    complete day15

---
+++ Day15/Program.cs
@@ -0,0 +1,93 @@
+﻿
+class InvalidAgeException : Exception
+{
+    public InvalidAgeException(string message) : base(message)
+    {
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            int a = 10;
+            int b = 0;
+            int result = a / b;   // ❌ Runtime error
+            Console.WriteLine(result);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Something went wrong!");
+            Console.WriteLine(ex.Message);
+        }
+
+        try
+        {
+            int number = int.Parse("abc");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid number format");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Cleanup done");
+        }
+
+        try
+        {
+            Withdraw(1000, 2000);
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        try
+        {
+            ValidateAge(12);
+        }
+        catch (InvalidAgeException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        try
+        {
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Square: {number * number}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Please enter a valid number");
+        }
+
+
+    }
+    static void Withdraw(double balance, double amount)
+    {
+        if (amount > balance)
+        {
+            throw new InvalidOperationException("Insufficient balance");
+        }
+
+        Console.WriteLine("Withdrawal successful");
+    }
+
+    static void ValidateAge(int age)
+    {
+        if (age < 18)
+        {
+            throw new InvalidAgeException("Age must be 18 or above");
+        }
+    }
+
+}
