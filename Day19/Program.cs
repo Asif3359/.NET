291c5e098be0587be16f042bd918caf5ac231af8
Asif3359 <asifahammed359@gmail.com>
2026-01-06 14:54:45 +0600

    Complete Day20

---
+++ Day19/Program.cs
@@ -0,0 +1,43 @@
+﻿
+class Student
+{
+    public string Name { get; set; }
+    public double CGPA { get; set; }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        List<int> numbers = new() { 2, 4, 6, 7, 9, 10 };
+        List<int> marks = new() { 20, 40, 60, 70, 90, 100 };
+
+        var even = numbers.Where(n => n % 2 == 0);
+
+        var squares = numbers.Select(n => n * n);
+
+        var sorted = numbers.OrderBy(n => n);
+        var desc = numbers.OrderByDescending(n => n);
+
+        var firstEven = numbers.First(n => n % 2 == 0);
+        var safeEven = numbers.FirstOrDefault(n => n % 2 == 0);
+
+        bool hasFail = marks.Any(m => m < 60);
+        bool allPassed = marks.All(m => m >= 60);
+
+        int count = numbers.Count();
+        int sum = numbers.Sum();
+        int max = numbers.Max();
+
+        List<Student> students = new()
+        {
+            new Student { Name = "Asif", CGPA = 3.8 },
+            new Student { Name = "Rahim", CGPA = 2.9 }
+        };
+        var toppers = students.Where(s => s.CGPA >= 3.5).Select(s => s.Name);
+        var result = students.Where(s => s.CGPA >= 3.0).OrderByDescending(s => s.CGPA).Select(s => s.Name);
+
+        
+
+    }
+}
---
+++ Day20/Program.cs
@@ -0,0 +1,34 @@
+﻿class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double CGPA { get; set; }
+    public bool IsActive { get; set; }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        List<Student> students = new()
+        {
+            new Student { Id = 1, Name = "Asif", CGPA = 3.85, IsActive = true },
+            new Student { Id = 2, Name = "Rahim", CGPA = 2.90, IsActive = false },
+            new Student { Id = 3, Name = "Karim", CGPA = 3.40, IsActive = true },
+            new Student { Id = 4, Name = "Sakib", CGPA = 3.95, IsActive = true },
+            new Student { Id = 5, Name = "Nabil", CGPA = 2.50, IsActive = false }
+        };
+
+        var isActive = students.Where(n => n.IsActive == true);
+        foreach (var item in isActive)
+        {
+            Console.WriteLine(item.Name);
+        }
+        Console.WriteLine("\n");
+        var sorted = students.OrderByDescending(n => n.CGPA);
+        Console.WriteLine(sorted.First().Name);
+
+
+
+    }
+}
