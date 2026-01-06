d034e3b417b9ae1b0cd91083a285c5a4f15bec4c
Asif3359 <asifahammed359@gmail.com>
2026-01-06 16:09:26 +0600

    Complete Day22

---
+++ Day21/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static async Task<string> GetMessageAsync()
+    {
+        await Task.Delay(2000);
+        return "Hello after delay";
+    }
+    static async Task<int> GetNumberAsync()
+    {
+        await Task.Delay(1000);
+        return 42;
+    }
+    static async Task<string> GetUserAsync()
+    {
+        await Task.Delay(1500); // simulate DB
+        return "Asif";
+    }
+
+    public static async Task Main()
+    {
+        string message = await GetMessageAsync();
+        Console.WriteLine(message);
+
+        int result = await GetNumberAsync();
+        Console.WriteLine(result);
+
+        string user = await GetUserAsync();
+        Console.WriteLine(user);
+    }
+}
---
+++ Day22/Program.cs
@@ -0,0 +1,50 @@
+﻿class Program
+{
+    static async Task<string> GetMessageAsync()
+    {
+        await Task.Delay(2000);
+        return "Hello after delay";
+    }
+    static async Task<int> GetNumberAsync()
+    {
+        await Task.Delay(1000);
+        return 42;
+    }
+    static async Task<string> GetUserAsync()
+    {
+        await Task.Delay(1500); // simulate DB
+        return "Asif";
+    }
+
+    public static async Task Main()
+    {
+        // string message = await GetMessageAsync();
+        // Console.WriteLine(message);
+
+        // int result = await GetNumberAsync();
+        // Console.WriteLine(result);
+
+        // string user = await GetUserAsync();
+        // Console.WriteLine(user);
+
+        Task task = Task.Run(() =>
+                    {
+                        Console.WriteLine("Running in background");
+                    });
+        await task;
+
+        Task t1 = Task.Delay(2000);
+        Task t2 = Task.Delay(3000);
+
+        await Task.WhenAll(t1, t2);
+
+        Console.WriteLine("Both done");
+
+
+        Parallel.ForEach(new[] { 1, 2, 3, 4 }, number =>
+        {
+            Console.WriteLine(number);
+        });
+
+    }
+}
