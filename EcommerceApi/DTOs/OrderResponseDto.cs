1d6033da3b9eec1fc432e5f5636abd481a34c3d4
Asif3359 <asifahammed359@gmail.com>
2026-01-17 00:14:33 +0600

    fix some bug

--- EcommerceApi/DTOs/OrderResponseDto.cs
+++ EcommerceApi/DTOs/OrderResponseDto.cs
@@ -8,15 +8,15 @@ namespace EcommerceApi.DTOs
 {
     public class OrderResponseDto
     {
-
         public long Id { get; set; }
         public string OrderNumber { get; set; } = string.Empty;
         public DateTime OrderDate { get; set; }
         public string Status { get; set; } = string.Empty;
         public double TotalAmount { get; set; }
         public string ShippingAddress { get; set; } = string.Empty;
-        public UserInfoDto User { get; set; } = new UserInfoDto();
-        public List<OrderItemResponseDto> Items { get; set; } = new List<OrderItemResponseDto>();
-
+        public long UserId { get; set; }
+        public string? UserName { get; set; }
+        public string? UserEmail { get; set; }
+        public List<OrderItemResponseDto> OrderItems { get; set; } = new List<OrderItemResponseDto>();
     }
 }
