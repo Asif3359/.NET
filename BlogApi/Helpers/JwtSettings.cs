8dbeb174637baed43845550d9f367abfda0612d9
Asif3359 <asifahammed359@gmail.com>
2026-01-23 00:13:50 +0600

    Add DTOs with validation

---
+++ BlogApi/Helpers/JwtSettings.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApi.Helpers
+{
+    public class JwtSettings
+    {
+        public string Secret { get; set; } = string.Empty;
+        public string Issuer { get; set; } = string.Empty;
+        public string Audience { get; set; } = string.Empty;
+        public int ExpirationInMinutes { get; set; } = 60;
+    }
+}
---
+++ BlogApi/Helpers/PaginationParameters.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApi.Helpers
+{
+    public class PaginationParameters
+    {
+        private const int MaxPageSize = 50;
+        private int _pageSize = 10;
+        public int PageNumber { get; set; } = 1;
+
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+
+    }
+
+
+    public class PagedResponse<T>
+    {
+        public IEnumerable<T> Data { get; set; } = new List<T>();
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalRecords { get; set; }
+
+        public bool HasPrevious => PageNumber > 1;
+        public bool HasNext => PageNumber < TotalPages;
+
+        public PagedResponse(IEnumerable<T> data, int count, int pageNumber, int pageSize)
+        {
+            Data = data;
+            TotalRecords = count;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        }
+    }
+
+}
