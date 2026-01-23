226a0acf7343270aab0c181e4acad12b76cc8521
Asif3359 <asifahammed359@gmail.com>
2026-01-23 11:25:45 +0600

    Add DTOs with validation

---
+++ BlogApi/DTOs/UpdatePostDto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogApi.DTOs
+{
+    public class UpdatePostDto
+    {
+        [StringLength(200, MinimumLength = 5)]
+        public string? Title { get; set; }
+
+        [StringLength(1000, MinimumLength = 10)]
+        public string? Content { get; set; }
+
+        public PostStatus? Status { get; set; }
+
+        public List<string>? Tags { get; set; }
+    }
+}
